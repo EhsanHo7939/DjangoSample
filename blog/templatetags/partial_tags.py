79ae1ce713b24162e2daf1d860ee5c3d80dd3e1d
Ehsan Ho <EhsanHo7939@gmail.com>
2022-02-08 22:56:44 +0330

    Active Link Tag

--- blog/templatetags/partial_tags.py
+++ blog/templatetags/partial_tags.py
@@ -8,3 +8,14 @@ def cat_tags():
     return {
         "categories": Category.objects.filter(status=True)
     }
+
+
+@register.inclusion_tag("registration/partials/active_link.html")
+def active_link(request, link_name, content, classes):
+    return {
+        "request": request,
+        "link_name": link_name,
+        "link": "account:{0}".format(link_name),
+        "classes": classes,
+        "content": content,
+    }
