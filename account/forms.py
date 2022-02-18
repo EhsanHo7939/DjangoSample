d9260af94465e712e0e5a06fbeab8cbab38d2bd7
Ehsan Ho <EhsanHo7939@gmail.com>
2022-02-18 11:22:51 +0330

    Superuser Exception for editing Profile

--- account/forms.py
+++ account/forms.py
@@ -4,13 +4,15 @@ from .models import User
 
 class ProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
         super(ProfileForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].disabled = True
-        self.fields['email'].disabled = True
-        self.fields['VIP_user'].disabled = True
-        self.fields['is_author'].disabled = True
-        self.fields['date_joined'].disabled = True
+        if not user.is_superuser:
+            self.fields['username'].disabled = True
+            self.fields['email'].disabled = True
+            self.fields['VIP_user'].disabled = True
+            self.fields['is_author'].disabled = True
+            self.fields['date_joined'].disabled = True
 
         self.fields['username'].help_text = None
 
