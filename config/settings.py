e6bc28199d0c00aa77c739c1fff453261cc79ac5
Ehsan Ho <EhsanHo7939@gmail.com>
2022-07-31 23:31:28 +0430

    Gmail smtp Backend config + python decouple

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ LOGIN_URL = "login"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%z#3k1w=%sc8@cir2j)6bsy21g!^*4kx@yq+$=_s02#co^l=yu'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,5 +153,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # User Authentication
 AUTH_USER_MODEL = 'account.User'
 
+
 # Email Backend
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
